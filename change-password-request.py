9d82bf79411d77f8e1d5b310a06280da8edbc33a
Roberto Messa <wardog1@gmail.com>
2025-09-14 19:30:48 -0300

    fix: parameters adjust

--- change-password-request.py
+++ change-password-request.py
@@ -6,7 +6,7 @@ import requests
 REQUEST_VERB = 'PATCH'
 API_TOKEN = 'cluster'
 RESOURCE_TOKEN = 'accounts'
-RESOURCE_ID = 'robertomessabrasil@gmail.com'
+RESOURCE_ID = 'youremailaccount'
 def main():
 
     load_manager_url = "https://eur-001.endpointer.com"
